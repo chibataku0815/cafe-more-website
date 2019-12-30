aad4718b55c59ca9567233142f5c06481eec4138
千葉工 <chibataku0815@gmail.com>
2019-12-31 04:21:56 +0900

    Setup contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
+import { FormComponent } from './form/form.component';
+
+import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
+    FormComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    FormsModule,
+    ReactiveFormsModule,
+    NgBootstrapFormValidationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
+import { FormControl, FormGroup, Validators, ErrorMessage } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  formGroup: FormGroup;
+
+  customErrorMessages: ErrorMessage[] = [
+    {
+      error: 'required',
+      format: (label, error) => `${label.toUpperCase()} は必須項目です`
+    }, {
+      error: 'pattern',
+      format: (label, error) => `${label.toUpperCase()} 入力された形式が違います`
+    }
+  ];
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      Name: new FormControl(''),
+      Email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+      ]),
+      DetailContent: new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formGroup);
+    const url = environment.slackWebHooks;
+    const data = {
+      type: 'mrkdwn',
+      text: `Cafe More Webサイトからのお問合せ \n 名前: ${this.formGroup.value.Name} \n メールアドレス${this.formGroup.value.Email} \n お問い合せ内容: ${this.formGroup.value.DetailContent} \n `
+    };
+    const xml = new XMLHttpRequest();
+    xml.open('POST', url, false);
+    xml.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    xml.send(`payload=${JSON.stringify(data)}`);
+  }
+
+}
