21f6192aaa0525f8171e5eb51fc041d5c4b0dcc1
千葉工 <chibataku0815@gmail.com>
2019-12-30 15:08:18 +0900

    Setup scss and Added test Componets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import * as WebFont from 'webfontloader';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'cafe-more-websaite';
